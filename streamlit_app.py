1e080a18ce84b2419e0803217c41a736ad1adb3a
cristianobreja-databasedeveloper <108023030+cristianobreja-databasedeveloper@users.noreply.github.com>
2022-06-22 12:38:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('KAle, Spinach & Rocket Smoothie')
-streamlit.text ('Hard-Boild Free-Range Egg')
+streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥗 KAle, Spinach & Rocket Smoothie')
+streamlit.text ('🐔 Hard-Boild Free-Range Egg')
+streamlit.text ('🥑🍞 Avocado Toast')
